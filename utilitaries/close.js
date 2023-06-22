514f0b242803e9a734be06f8ff8b2e3f6f4029ba
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2023-06-22 14:51:55 +0200

    refact + add logs

--- utilitaries/close.js
+++ utilitaries/close.js
@@ -1,23 +1,16 @@
 const DS = require('discord.js')
-
-const admin = "1113951433365127318"
-const modo = "1113453048925265970"
-const support = "1113950111253413929"
+const { authorized } = require('./privilegied')
 
 module.exports = {
     closeCmd: async (bot, it) => {
-        const needContains = ["📄", "𝐓𝐢𝐜𝐤𝐞𝐭"]
+        const needContains = ["📄", "°"]
         const channelToDelete = await bot.channels.cache.find(channel => channel.id === it.channelId)
 
-        const authorized = [admin, modo, support]
         let flag = false
         let canDelete = true
         authorized.forEach((r)=>{
-            console.log(r)
-            console.log(it.member._roles.includes(r))
             if(it.member._roles.includes(r)){
                 flag = true
-                console.log("HERE 1")
                 needContains.forEach((s)=> {
                     if (!channelToDelete.name.includes(s)) {
                         flag = false;
@@ -29,6 +22,9 @@ module.exports = {
             }
         })
         if(!flag && !canDelete) it.reply("Vous n'avez pas les droit suffisant pour faire cela")
-        else channelToDelete.delete()
+        else {
+            console.log("Deleted channel:  " + channelToDelete.name + " by user: " + it.user.username)
+            channelToDelete.delete()
+        }
     }
 }
---
+++ utilitaries/privilegied.js
@@ -0,0 +1,7 @@
+const admin = "1113951433365127318"
+const modo = "1113453048925265970"
+const support = "1113950111253413929"
+
+module.exports = {
+    authorized: [admin, modo, support]
+}

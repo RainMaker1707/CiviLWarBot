42887e5e311bb3e38abba27cc375ef342384d023
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2023-06-29 12:12:43 +0200

    Modify ticket to avoid timing error

--- utilitaries/ticketDeath.js
+++ utilitaries/ticketDeath.js
@@ -3,16 +3,18 @@ const { answer } = require("./ticketAnswer");
 
 
 module.exports = {
-    ticketDeath: async (bot, id) => { 
-        const channelColl = await bot.channels.cache.find(channel => channel.id === id)
-        const collector = channelColl.createMessageComponentCollector({
-            componentType: ComponentType.Button,
-        });
-        collector.on("collect", async (it)=>{
-            switch (it.customId){
-                case "rp_death_ticket": answer(it, "📄📘┃𝐓𝐢𝐜𝐤𝐞𝐭-𝐌°", "Comment etes vous mort? Une mort RP signifie un wipe du personnage et de son histoire, vous devrez impérativement en jouer un nouveau", bot, id); break;
-                case "death_ask_ticket": answer(it, "📄📘┃𝐓𝐢𝐜𝐤𝐞𝐭-𝐃°", "Déposer votre dossier de mort RP ici, nous le traiterons le plus vite possible", bot, id); break;
-            }
+    ticketDeath: (bot, id) => { 
+        const channelColl = bot.channels.fetch(id)
+        channelColl.then((chan)=>{
+            const collector = chan.createMessageComponentCollector({
+                componentType: ComponentType.Button,
+            });
+            collector.on("collect", (it)=>{
+                switch (it.customId){
+                    case "rp_death_ticket": answer(it, "📄📘┃𝐓𝐢𝐜𝐤𝐞𝐭-𝐌°", "Comment etes vous mort? Une mort RP signifie un wipe du personnage et de son histoire, vous devrez impérativement en jouer un nouveau", bot, id); break;
+                    case "death_ask_ticket": answer(it, "📄📘┃𝐓𝐢𝐜𝐤𝐞𝐭-𝐃°", "Déposer votre dossier de mort RP ici, nous le traiterons le plus vite possible", bot, id); break;
+                }
+            })
         })
     }
 }
--- utilitaries/ticketGlobal.js
+++ utilitaries/ticketGlobal.js
@@ -2,17 +2,18 @@ const DS = require('discord.js');
 const { ComponentType } = require('discord.js');
 const { answer } = require("./ticketAnswer");
 
-let counter = 0
 
 module.exports = {
-    ticketGlobal: async (bot, name, id, message) => { 
-        const channelColl = await bot.channels.cache.find(channel => channel.id === id)
-        const collector = channelColl.createMessageComponentCollector({
-            componentType: ComponentType.Button,
-        });
-
-        collector.on("collect", async (it)=>{
-            answer(it, name, message, bot, id);
+    ticketGlobal: (bot, name, id, message) => { 
+        const channelColl = bot.channels.fetch(id)
+        channelColl.then((chan)=>{
+            const collector = chan.createMessageComponentCollector({
+                componentType: ComponentType.Button,
+            });
+    
+            collector.on("collect", (it)=>{
+                answer(it, name, message, bot, id);
+            })
         })  
     }
 }

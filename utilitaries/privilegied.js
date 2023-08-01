43c49abcd36dae1b283910406b706a20449b0503
LieutenantMasterRemote <perso@guillaumejouve.fr>
2023-08-01 10:13:55 +0200

    Add privilegied role for passeport

--- utilitaries/privilegied.js
+++ utilitaries/privilegied.js
@@ -3,9 +3,11 @@ const modo = "1113453048925265970"
 const support = "1113950111253413929"
 const whitelisted = "1113952117435146370"
 const nonwhitelisted = "1113453224133939210"
+const gouvernment = "1135671416419778590"
 
 module.exports = {
     authorized: [admin, modo, support],
+    authorized_passport: [admin, modo, support, gouvernment],
     admin: admin,
     modo: modo,
     support: support,
